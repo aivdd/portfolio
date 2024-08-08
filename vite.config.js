c7ad2e7248679fba54f0dd1310d36bff78549ea4
David Vid <davidyusufrahmanto@gmail.com>
2024-08-09 01:59:32 +0700

    add: deploy

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio/',
   css: {
     postcss: {
       plugins: [tailwindcss(), autoprefixer()],
