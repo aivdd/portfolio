a8465fe77a2e7c96c14d7583a48edde4b5ac5b66
David Vid <davidyusufrahmanto@gmail.com>
2024-08-09 02:09:03 +0700

    fix: image profile

--- src/components/ImageProfile.jsx
+++ src/components/ImageProfile.jsx
@@ -3,7 +3,7 @@ import { Avatar, Box } from '@radix-ui/themes';
 const ImageProfile = () => {
   return (
     <Avatar
-      src='/Profile-Photo-DSC08551.JPG'
+      src='./Profile-Photo-DSC08551.JPG'
       style={{
         width: '320px',
         height: '320px',
